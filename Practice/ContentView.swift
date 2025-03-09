e2658b315323d0cfe31543ca794847a447e08fe1
seahpark247 <famousjedo@gmail.com>
2025-03-09 14:43:23 -0500

    practice - GuessTheFlag

--- Practice/ContentView.swift
+++ Practice/ContentView.swift
@@ -7,66 +7,121 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    @State private var money = 0.0
-    @State private var people = 2
-    @State private var tipPercentage = 20
-    @FocusState private var isFocused: Bool
-    
-    let tipPercentages = [10, 15, 18, 20, 0]
-    
-    var checkAmount: Double {
-        let tip = money / 100 * Double(tipPercentage)
-        return money + tip
-    }
-    
-    var checkPerPerson: Double {
-        return checkAmount / Double(people + 2)
-    }
+struct FlagImage: View {
+    var imageName: String
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                Text("").frame(maxWidth: .infinity, maxHeight: .infinity).background(.green.gradient).ignoresSafeArea()
+        Image(imageName)
+            .clipShape(.capsule)
+            .shadow(radius: 5)
+    }
+}
+
+struct ContentView: View {
+    @State private var countries = ["Estonia", "France", "Germany", "Ireland", "Italy", "Nigeria", "Poland", "Spain", "UK", "Ukraine", "US", "Korea"].shuffled()
+    @State private var correctAnswer = Int.random(in: 0...2)
+    
+    @State private var showingScore = false
+    @State private var scoreTitle = ""
+    @State private var score = 0
+    @State private var gameCount = 0
+
+    var body: some View {
+        ZStack {
+            RadialGradient(
+                stops: [.init(color: Color(red: 0.1, green: 0.2, blue: 0.45), location: 0.3),
+                        .init(color: Color(red: 0.76, green: 0.15, blue: 0.26), location: 0.3)],
+                center: .top, startRadius: 200, endRadius: 700
+            ).ignoresSafeArea()
+            
+            VStack {
+                Spacer()
                 
-                Form {
-                    Section("How much money") {
-                        TextField("Amount", value: $money, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
+                Text("Guess the Flag")
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+                
+                VStack(spacing: 15) {
+                    VStack {
+                        Text("Tap the flag of")
+                            .foregroundStyle(.secondary)
+                            .font(.subheadline.bold())
                         
-                        Picker("Number of people", selection: $people) {
-                            ForEach(2..<100) {
-                                Text("\($0) people")
-                            }
-                        }
+                        Text(countries[correctAnswer])
+                            .font(.largeTitle.weight(.semibold))
                     }
                     
-                    Section("How much tip?") {
-                        Picker("Tip percentage", selection: $tipPercentage) {
-                            ForEach(tipPercentages, id: \.self) {
-                                Text($0, format: .percent)
-                            }
-                        }.pickerStyle(.segmented)
-                    }
-                    
-                    Section("Amount for check") {
-                        Text(checkAmount, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    }
-                    
-                    Section("Amout per person") {
-                        Text(checkPerPerson, format: .currency(code: Locale.current.currency?.identifier ?? "USD"))
-                    }
-                }
-                .navigationTitle(Text("WeSplit6"))
-                .toolbar {
-                    if isFocused {
-                        Button("Done") {
-                            isFocused = false
+                    ForEach(0..<3) { number in
+                        Button {
+                            flagTapped(number)
+                        } label: {
+                            FlagImage(imageName: countries[number])
                         }
                     }
                 }
+                .frame(maxWidth: .infinity)
+                .padding(.vertical, 20)
+                .background(.regularMaterial)
+                .clipShape(.rect(cornerRadius: 20))
+                
+                Spacer()
+                Spacer()
+                
+                Text("Score: \(score)")
+                    .foregroundStyle(.white)
+                    .font(.title.bold())
+                
+                Spacer()
             }
+            .padding()
         }
-        .scrollContentBackground(.hidden)
+        .alert(scoreTitle, isPresented: $showingScore) {
+            if gameCount == 8 {
+                Button("Restart", action: reStart)
+            } else {
+                Button("Continue", action: askQuestion)
+            }
+        } message: {
+            Text("Your current score is \(score). Played: \(gameCount)/8")
+        }
+    }
+    
+    func flagTapped(_ number: Int) {
+        gameCount += 1
+        
+        if number == correctAnswer {
+            scoreTitle = "Correct!"
+            score += 1
+        } else {
+            scoreTitle = "Wrong! That's the flag of \(countries[number])"
+        }
+        
+        if gameCount == 8 {
+            var judging = ""
+            
+            if score < 3 {
+                judging = "work hard!"
+            } else if score < 6 {
+                judging = "not bad!"
+            } else {
+                judging = "good job!"
+            }
+            
+            scoreTitle += " And Game over! \(judging)"
+        }
+        
+        showingScore = true
+    }
+    
+    func askQuestion() {
+        countries.shuffle()
+        correctAnswer = Int.random(in: 0...2)
+    }
+    
+    func reStart() {
+        askQuestion()
+        score = 0
+        gameCount = 0
     }
 }
 
