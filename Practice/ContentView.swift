5814af00521e4d1947ed50d366e54951c8221d97
seahpark247 <famousjedo@gmail.com>
2025-03-15 16:38:19 -0500

    practice - ViewModifiers

--- Practice/ContentView.swift
+++ Practice/ContentView.swift
@@ -7,134 +7,41 @@
 
 import SwiftUI
 
-enum Player {
-    case x, o, none
-}
-
-struct Cell: ViewModifier {
+struct BlueButton: ViewModifier {
     func body(content: Content) -> some View {
         content
-            .font(.system(size: 50))
-            .foregroundColor(.black)
-            .frame(width: 80, height: 80)
-            .background(.black.opacity(0.3))
-            .cornerRadius(10)
-    }
-}
-
-struct RestartButton: ViewModifier {
-    func body(content: Content) -> some View {
-        content
-            .font(.title2)
+            .font(.largeTitle.bold())
             .foregroundColor(.white)
             .padding()
-            .background(.black.gradient)
-            .cornerRadius(10)
+            .background(.blue)
     }
 }
 
-struct StatusText: ViewModifier {
+struct CapsuleButton: ViewModifier {
     func body(content: Content) -> some View {
         content
-            .font(.title2.bold())
-            .foregroundColor(.white)
+            .font(.largeTitle.bold())
+            .foregroundColor(.blue)
+            .padding()
+            .background(.yellow)
+            .clipShape(.capsule)
     }
 }
 
 extension View {
-    func cellStyle() -> some View {
-        modifier(Cell())
-    }
-    
     func buttonStyle() -> some View {
-        modifier(RestartButton())
+        modifier(BlueButton())
     }
     
-    func statusStyle() -> some View {
-        modifier(StatusText())
+    func capsuleStyle() -> some View {
+        modifier(CapsuleButton())
     }
 }
 
 struct ContentView: View {
-    @State private var board: [[Player]] = Array(repeating: Array(repeating: .none, count: 3), count: 3)
-    @State private var currentPlayer: Player = .x
     
     var body: some View {
-        NavigationStack {
-            ZStack {
-                Text("").frame(maxWidth: .infinity, maxHeight: .infinity).background(.indigo.gradient).ignoresSafeArea()
-            
-                VStack {
-                    HStack {
-                        Spacer()
-                        VStack {
-                            ForEach(0..<3) { col in
-                                HStack {
-                                    ForEach(0..<3) { row in
-                                        Button {
-                                            makeMove(row: row, col: col)
-                                        } label: {
-                                            // Text(board[row][col] == .x ? "X" : board[row][col] == .o ? "O" : "").cellStyle()! 텍스트칸에 보드 체크해서 엑스 갖고있으면 엑스 넣어주는 식!!
-                                            Text(board[row][col] == .x ? "X" : board[row][col] == .o ? "O" : "").cellStyle()
-                                        }
-                                    }
-                                }
-                            }
-                        }
-                        Spacer()
-                    }
-                    .padding(.vertical, 30)
-                    .background(.regularMaterial)
-                    
-                    HStack {
-                        Button("Restart", action: resetBoard)
-                            .buttonStyle()
-                        Spacer()
-                        Text(checkWinner(player: .x) ? "X is won!" : checkWinner(player: .o) ? "O is won!" : isDraw() ? "Draw!" : "Playing...").statusStyle()
-                    }.padding()
-                }
-            }.navigationTitle("TicTacToe")
-        }.scrollContentBackground(.hidden)
-    }
-    
-    func makeMove(row: Int, col: Int) {
-        guard board[row][col] == .none && !checkWinner(player: .x) && !checkWinner(player: .o) else {
-            return
-        }
-        
-        board[row][col] = currentPlayer
-        currentPlayer = currentPlayer == .x ? .o : .x
-    }
-    
-    func checkWinner(player: Player) -> Bool {
-        for i in 0..<3 {
-            if board[i][0] == player && board[i][1] == player && board[i][2] == player {
-                return true
-            }
-            if board[0][i] == player && board[1][i] == player && board[2][i] == player {
-                return true
-            }
-        }
-        
-        if board[0][0] == player && board[1][1] == player && board[2][2] == player {
-            return true
-        }
-        if board[0][2] == player && board[1][1] == player && board[2][0] == player {
-            return true
-        }
-        
-        return false
-    }
-    
-    func isDraw() -> Bool {
-        board.allSatisfy { row in
-            row.allSatisfy { $0 != .none }
-        }
-    }
-    
-    func resetBoard() {
-        board = Array(repeating: Array(repeating: .none, count: 3), count: 3)
-        currentPlayer = .x
+        Text("dd")
     }
     
 }
