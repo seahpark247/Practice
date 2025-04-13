1746da68c67290f99dc8e5a87626a7b014d90a24
seahpark247 <famousjedo@gmail.com>
2025-04-13 14:33:45 -0500

    refactoring: code

--- Practice/ContentView.swift
+++ Practice/ContentView.swift
@@ -55,7 +55,10 @@ enum TempType: String, CaseIterable {
 struct ContentView: View {
     @State private var myTemp: TempType = .celsius
     @State private var input: Double = 0.0
-    @State private var output: Double = 0.0
+    
+    var convertedOutput: Double {
+        myTemp.convert(input)
+    }
     
     var tempColor: Color {
         myTemp.color(input)
@@ -74,7 +77,6 @@ struct ContentView: View {
                             ForEach(TempType.allCases, id: \.self) { Text($0.rawValue) }
                         }
                         .pickerStyle(.segmented)
-                        .onChange(of: myTemp) { tempConvert() }
                     }
                     .padding(.top)
                     .listRowBackground(Color.clear)
@@ -82,21 +84,20 @@ struct ContentView: View {
                     Section(myTemp.symbol) {
                         TextField(myTemp.rawValue, value: $input, format: .number)
                             .keyboardType(.decimalPad)
-                            .onChange(of: input) { tempConvert() }
                             .multilineTextAlignment(.center)
                     }
                 
                     Section {
                         HStack {
                             Spacer()
-                            Image(systemName: "arrow.triangle.2.circlepath")
+                            Image(systemName: "arrow.down")
                             Spacer()
                         }
                     }
                     .listRowBackground(Color.clear)
                 
                     Section(myTemp.opposite.symbol) {
-                        Text(String(format: "%.2f", output)).frame(maxWidth: .infinity, alignment: .center)
+                        Text(String(format: "%.2f", convertedOutput)).frame(maxWidth: .infinity, alignment: .center)
                     }
                 }
                 .navigationTitle("TempConv")
@@ -105,10 +106,6 @@ struct ContentView: View {
         }
     }
     
-    func tempConvert() {
-        output = myTemp.convert(input)
-    }
-
 }
    
 #Preview {
