fc97517b9d764b29ead21680dcf444716b6c46e6
seahpark247 <famousjedo@gmail.com>
2025-05-20 07:29:04 -0500

    practice: UserDefaults

--- Practice/ContentView.swift
+++ Practice/ContentView.swift
@@ -8,19 +8,22 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var memo: String = ""
+    @State private var dream = ""
+    @State private var yesterdayDream = ""
     
     var body: some View {
         VStack {
-            TextField("Enter text", text: $memo)
+            TextField("Enter your dream today...", text: $dream)
             
             Button("Save") {
-                UserDefaults.standard.set(memo, forKey: "savedMemo")
+                UserDefaults.standard.set(dream, forKey: "savedDream")
             }
             
-            Button("Road") {
-                memo = UserDefaults.standard.string(forKey: "savedMemo") ?? "Saved memo not found"
+            Button("Yesterday's Dream") {
+                yesterdayDream = UserDefaults.standard.string(forKey: "savedDream") ?? "No dream saved yesterday"
             }
+            
+            Text(yesterdayDream)
         }
     }
     
