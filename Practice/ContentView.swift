24d128cf355828e70170e260b7806079f1ed4c69
seahpark247 <famousjedo@gmail.com>
2025-05-26 19:25:27 -0500

    practice: UserDefaults

--- Practice/ContentView.swift
+++ Practice/ContentView.swift
@@ -8,22 +8,30 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State private var dream = ""
-    @State private var yesterdayDream = ""
+    @State private var toughts = ""
+    @State private var yesterday = ""
     
     var body: some View {
-        VStack {
-            TextField("Enter your dream today...", text: $dream)
-            
-            Button("Save") {
-                UserDefaults.standard.set(dream, forKey: "savedDream")
-            }
-            
-            Button("Yesterday's Dream") {
-                yesterdayDream = UserDefaults.standard.string(forKey: "savedDream") ?? "No dream saved yesterday"
-            }
-            
-            Text(yesterdayDream)
+        NavigationStack {
+            Form {
+                Section {
+                    TextField("input your toughts today", text: $toughts)
+                    HStack {
+                        Spacer()
+                        Button("Save your toughts") {
+                            UserDefaults.standard.set(toughts, forKey: "toughts")
+                        }
+                    }
+                }
+                
+                Section("saved thoughts") {
+                    Button("Check if there is any") {
+                        // 유저 디폴트 값 불러오고-> 없으면 ?? 뒤에 비었다고 하기
+                        yesterday = UserDefaults.standard.string(forKey: "toughts") ?? "It's empty yet"
+                    }
+                    Text(yesterday)
+                }
+            }.navigationTitle("UserDefaults")
         }
     }
     
